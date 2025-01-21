43ae428e2a073222f8b7e61d3756ccc075521656
Jay Pandya <113614843+Infern07@users.noreply.github.com>
2025-01-21 09:01:53 +0530

    Update 1.variables.js

--- 01_basics/1.variables.js
+++ 01_basics/1.variables.js
@@ -20,8 +20,16 @@ alert("hope you enjoyed this little shit");
 var ans = prompt("waant mmoreeeee? (type yes/no)");
 if(ans == "yes" || ans=="YES" || ans=="Yes"){
     alert("she said yes you guys... 🙏");
+    alert("holy shit idk what to say now")
+    alert("AAAAAAAAAAAAA wtf")
+    alert("we in a relationship now ig")
+    alert("is this what it feels like?")
+    alert("am i pregnant now?")
+    alert("wait no-")
+    alert("are you pregnant now?")
+    alert("what???")
 }
-alert("also, ofcourse you do, everyone wants more of me");
+alert("also, ofcourse you want more, everyone wants more of me");
 alert("but dw ham sirf apke to, stay tuned...");
 alert("like i do");
 alert("for your messages and calls");
